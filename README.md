6147dadf649d9d7a3677d922357fb89d78167d91
James McDonnell <james@finetunelearning.com>
2020-12-25 01:28:14 -0800

    Adding script and readme.

---
+++ README.md
@@ -0,0 +1,46 @@
+# Script to pull transaction information from RBC pdfs
+
+I wrote this a couple of years ago, it has problems with H's and ends up recognizing them as 11, and some other small issues.
+
+I give no guarantee about accuracy of the output files vs the input. This uses character recognition, I suggest you compare the data after running the script.
+
+I really only run it yearly to do an audit for taxes. I can't quite recall what the proper requirments.txt values are. As well as I think there are some specific libraries needed outside of pip for the ocr.
+
+Usage:
+
+    rbc_ocr.py "PATH/TO/PDF/FILE"
+
+the packages I have installed in my virtualenv are:
+
+    image==1.5.27
+      - django [required: Any, installed: 1.11.18]
+        - pytz [required: Any, installed: 2018.9]
+      - pillow [required: Any, installed: 5.4.1]
+    matplotlib==2.2.3
+      - backports.functools-lru-cache [required: Any, installed: 1.5]
+      - cycler [required: >=0.10, installed: 0.10.0]
+        - six [required: Any, installed: 1.12.0]
+      - kiwisolver [required: >=1.0.1, installed: 1.0.1]
+        - setuptools [required: Any, installed: 44.1.1]
+      - numpy [required: >=1.7.1, installed: 1.16.0]
+      - pyparsing [required: >=2.0.1,!=2.1.6,!=2.1.2,!=2.0.4, installed: 2.3.1]
+      - python-dateutil [required: >=2.1, installed: 2.7.5]
+        - six [required: >=1.5, installed: 1.12.0]
+      - pytz [required: Any, installed: 2018.9]
+      - six [required: >=1.10, installed: 1.12.0]
+      - subprocess32 [required: Any, installed: 3.5.3]
+    opencv-python==4.0.0.21
+      - numpy [required: >=1.11.1, installed: 1.16.0]
+    pdf2image==1.4.0
+      - pillow [required: Any, installed: 5.4.1]
+    pip==20.3.3
+    pkg-resources==0.0.0
+    pyocr==0.5.3
+      - Pillow [required: Any, installed: 5.4.1]
+      - six [required: Any, installed: 1.12.0]
+    tesseract==0.1.3
+    unicodecsv==0.14.1
+    Wand==0.5.0
+    wheel==0.36.2
+
+There was some specifics I needed to get tesseract/pyocr installed, hopefully the above helps.
---
+++ rbc_ocr.py
@@ -0,0 +1,122 @@
+#!venv/bin/python
+import sys
+from PIL import Image
+import pyocr
+from wand.image import Image as Img
+import pyocr.builders
+from pdf2image import convert_from_path
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import unicodecsv as csv
+import re
+import os
+
+template = cv2.imread('divider.jpg',0)
+w, h = template.shape[::-1]
+
+
+tools = pyocr.get_available_tools()[0]
+
+
+file=os.path.abspath(sys.argv[1])
+print(file)
+filename=file.split("/")[-1]
+try:
+    os.mkdir(".ocr")
+except OSError:  
+    pass
+
+#convert pdf to img
+pages = convert_from_path(file, 500)
+print(pages)
+i = 0
+files = []
+output = []
+for page in pages:
+	i = i + 1
+	page_filename = ".ocr/" + filename + ".%s.png" % i
+	page.save(page_filename, 'PNG')
+	files = files + [page_filename]
+	
+	#divider recognition start
+	img_rgb = cv2.imread(page_filename)
+	img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+	
+	ret, thresh = cv2.threshold(img_gray,127,255,cv2.THRESH_BINARY)
+	frame = cv2.GaussianBlur(img_gray, (0, 0), 10);
+	thresh = cv2.addWeighted(img_gray, 1.5, frame, -0.5, 0);
+	cv2.imwrite(page_filename + '.out.png',thresh)
+	
+	res = cv2.matchTemplate(thresh,template,cv2.TM_CCORR_NORMED)
+	threshold = 0.8
+	loc = np.where( res >= threshold)
+	pts = zip(*loc[::-1])
+	
+	# collect ycoords of separators
+	ycoords = []
+	for pt in pts:
+		add = True
+		y = pt[1]
+		for stored_y in ycoords:
+			if abs(y - stored_y) < 5:
+				add = False
+				break
+		if add:
+			ycoords = ycoords + [pt[1]]
+	if len(ycoords) == 0:
+		continue
+	#add the last row as it is missing a bottom border.
+	ycoords = ycoords + [ycoords[-1] + 210 + 5]
+	# print(ycoords)
+	img = Image.open(page_filename + '.out.png')
+	for i in range(len(ycoords)-1):
+		#crop to transaction size
+		box = img.getbbox()
+		crop_img = img.crop((415, 0, 2415, box[3]))
+		box = crop_img.getbbox()
+		
+		# crop to individual transaction
+		crop_img = crop_img.crop((0, ycoords[i]+5, box[2], ycoords[i+1]))
+		
+		#get date
+		box = crop_img.getbbox()
+		trans_date = crop_img.crop((0,box[1]+5, 240, box[3]))
+		trans_date.save("foobar.png")
+		dt = tools.image_to_string(trans_date, builder=pyocr.builders.TextBuilder())
+		
+		#get details
+		details = crop_img.crop((440, box[1], box[2], box[3]))
+		text = tools.image_to_string(details, builder=pyocr.builders.TextBuilder())
+		lines = filter(None, text.split("\n"))
+		dollar_pos = lines[0].find("$")
+		negative = False
+		if lines[0][dollar_pos-1] == "-":
+			negative = True
+		foreign_exchange_line = lines[2] if len(lines) > 2 else ""
+		currency = None
+		exchange_rate = None
+		extra = ""
+		if len(foreign_exchange_line) > 0:
+			foreign_exchange_line = re.sub(r'q(?!u)', 'g', foreign_exchange_line)
+			try:
+				currency = foreign_exchange_line.split("Exchange rate-")[0].replace('Foreign Currency-', '')
+				exchange_rate = foreign_exchange_line.split("Exchange rate-")[1]
+			except IndexError:
+				print("Could not find Exchange rate: IndexError")
+				extra = foreign_exchange_line
+		d = {
+			"date": dt,
+			"description": lines[0].split("$")[0],
+			"amount": ("-" if negative else "") + lines[0].split("$")[1],
+			"transaction_number": lines[1] if len(lines) > 2 else "",
+			"exchange_rate": exchange_rate if exchange_rate is not None else "",
+			"foreign_currency": currency if currency is not None else "",
+			"extra": extra
+		}
+		output = output + [d]
+with open(filename.replace(".pdf", ".csv"), mode="w") as csv_file:
+	writer = csv.DictWriter(csv_file, delimiter=",", fieldnames=output[0].keys())
+	writer.writeheader()
+	for trans in output:
+		writer.writerow(trans)
